0e7785a15c495abd1b8667a82a512cb718bf1663
Romana <romana.leitgeb247@gmail.com>
2022-04-14 13:55:40 +0200

    my project finalized

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
+function Message1(){
+    document.getElementById("result").classList.remove("hidden");
+}
+
+
+function updateRate(){
+    var rateval=document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval; 
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    if(parseInt(principal)<1){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+    if(principal == ""){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var rate=document.getElementById("rate").value;
+    var years=document.getElementById("years").value;
+
+    var interest=principal*years*rate/100;
+
+    var today = new Date();    
+    var calcyear= parseInt(today.getFullYear())+parseInt(years);
     
+    document.getElementById("principalmessage").innerHTML = ""+principal;
+	document.getElementById("ratemessage").innerHTML = ""+rate;
+	document.getElementById("amountmessage").innerHTML = ""+interest;
+	document.getElementById("yearmessage").innerHTML = ""+calcyear;
+	Message1();
+
+	return false;
 }
-        
+
+
